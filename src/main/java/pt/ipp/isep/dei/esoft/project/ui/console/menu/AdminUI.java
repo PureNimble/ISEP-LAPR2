ee244ef88c834a7f9c0d45a85a1f19920fd93763
Diogo <1221014@isep.ipp.pt>
2023-04-28 20:45:44 +0100

    US03-Adding announcementRequest UI

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/AdminUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/AdminUI.java
@@ -1,7 +1,6 @@
 package pt.ipp.isep.dei.esoft.project.ui.console.menu;
 
 
-import pt.ipp.isep.dei.esoft.project.ui.console.CreateTaskUI;
 import pt.ipp.isep.dei.esoft.project.ui.console.ShowTextUI;
 import pt.ipp.isep.dei.esoft.project.ui.console.utils.Utils;
 
