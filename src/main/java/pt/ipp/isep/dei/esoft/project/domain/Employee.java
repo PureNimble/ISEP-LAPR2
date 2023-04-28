fae0bd970eaa908d0656b0ccf3cfb0853505823f
Diogo <1221014@isep.ipp.pt>
2023-04-28 23:15:52 +0100

    US3- Correcting error toString Employee

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/Employee.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/Employee.java
@@ -29,7 +29,7 @@ public class Employee extends User {
     }
 
     public String toString(){
-        return String.format(super.toString() + "\nfuncionário da loja %s que desempenha o papel de %s",store.toString(),roles.toString());
+        return String.format(super.toString() + "\nfuncionário da  %s que desempenha o papel de %s",store.toString(),roles.toString());
     }
 
     @Override
