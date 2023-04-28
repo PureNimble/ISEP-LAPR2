d35fddb92b4141ad824e86b3b9e41f698fc509db
Diogo <1221014@isep.ipp.pt>
2023-04-28 21:14:04 +0100

    Updating Role

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/Role.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/Role.java
@@ -9,20 +9,14 @@ import java.util.Objects;
 public class Role {
     private final String description;
 
-    /**
-
-     Creates a new role with the given description.
-     @param description The description of the role.
-     */
     public Role(String description) {
         this.description = description;
     }
-    /**
 
-     Checks if the current role is equal to the specified object.
-     @param o The object to compare.
-     @return true if the object is a Role and has the same description as the current role, false otherwise.
-     */
+    public String toString(){
+        return String.format(description);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -34,27 +28,25 @@ public class Role {
         Role that = (Role) o;
         return description.equals(that.description);
     }
-    /**
 
-     Returns the hash code of the current role.
-     @return The hash code of the current role.
-     */
     @Override
     public int hashCode() {
         return Objects.hash(description);
     }
-    /**
 
-     Returns the description of the role.
-     @return The description of the role.
+    /**
+     * This method returns the description of the task category.
+     *
+     * @return The description of the task category.
      */
     public String getDescription() {
         return description;
     }
-    /**
 
-     Creates a clone of the current role.
-     @return A clone of the current role.
+    /**
+     * Clone method.
+     *
+     * @return A clone of the current task.
      */
     public Role clone() {
         return new Role(this.description);
