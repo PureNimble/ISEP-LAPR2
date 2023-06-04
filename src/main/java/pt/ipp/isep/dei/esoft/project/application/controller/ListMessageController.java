d929d1da57b07b9ad1cf9f758a5fdba564a3c7b6
pedrom2004_ <1220688@isep.ipp.pt>
2023-06-04 19:18:49 +0100

    US010/US015/US016 -
    - Started creating JavaFX for these User Stories, Created class: AgentMenuGUI and AgentMenuGUI.fxml.
    - Implemented new methods in the following classes: AnnouncementRequestUI, Bootstrap, MainAppGUI, MainMenuGUI, MessageRepository, PublishAnnouncementUI and SendMessageUI.
    - Added the configuration file for US016.
    - Refinements to US010 classes and methods (not completed yet, 2/3 AC left).

--- src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/ListMessageController.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/ListMessageController.java
@@ -2,6 +2,8 @@ package pt.ipp.isep.dei.esoft.project.application.controller;
 
 
 import pt.ipp.isep.dei.esoft.project.domain.Message;
+import pt.ipp.isep.dei.esoft.project.domain.MessageDto;
+import pt.ipp.isep.dei.esoft.project.domain.MessageMapper;
 import pt.ipp.isep.dei.esoft.project.repository.*;
 
 import java.util.*;
@@ -9,6 +11,19 @@ import java.util.*;
 public class ListMessageController {
     private MessageRepository messageRepository = new MessageRepository();
     private UserRepository userRepository;
+
+    public ListMessageController() {
+        getMessageRepository();
+    }
+
+    private MessageRepository getMessageRepository() {
+        if (messageRepository == null) {
+            Repositories repositories = Repositories.getInstance();
+
+            messageRepository = repositories.getMessageRepository();
+        }
+        return messageRepository;
+    }
 
     public void sortBookingRequests(List<Message> bookingRequests) {
         Collections.sort(bookingRequests, Comparator.comparing(Message::getInitialDate));
@@ -23,5 +38,17 @@ public class ListMessageController {
         }
         return bookingRequests;
     }
+
+    public List<Message> getMessagesByAscendingDate() {
+        MessageRepository messageRepository = getMessageRepository();
+        return messageRepository.getMessagesByAscendingDate();
+    }
+
+    public List<MessageDto> toDtoAscendingArea() {
+        MessageMapper messageMapper = new MessageMapper();
+
+        return messageMapper.toDto(getMessagesByAscendingDate());
+    }
+
 
 }
