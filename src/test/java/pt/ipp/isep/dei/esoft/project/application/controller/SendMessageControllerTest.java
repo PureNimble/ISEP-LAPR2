42a2e9ca5436e47541d894e68dbc93d61126f2f6
Diogo <1221014@isep.ipp.pt>
2023-06-13 08:40:36 +0100

    Changing SendMessageControllerTest correcting errors

--- src/test/java/pt/ipp/isep/dei/esoft/project/application/controller/SendMessageControllerTest.java
+++ src/test/java/pt/ipp/isep/dei/esoft/project/application/controller/SendMessageControllerTest.java
@@ -21,12 +21,23 @@ class SendMessageControllerTest {
     int initialTime = 10;
     int endTime = 11;
     Comission com = new Comission(25.00);
-    Property property = new Property(2, 2);
+
+    Store store = new Store("Holloway",10234,address,1234567890,"holloway@gmail.com", 0);
+
+    Role role = new Role("Agent");
+
+    List<Role> roles = new ArrayList<>();
+
+    Employee employee =  new Employee("age@this.app", 123446789, 987658321, "Miguelito", 1234587890L, store,  roles.add(role), address);
+
+    Address address = new Address("Main Street", 1234, new District("Test District"), new City("Test City"), new State("Test State"));
+
+    Property property = new Property(274,55,address);
     PropertyType propertyType = new PropertyType("House");
     TypeOfBusiness typeOfBusiness = new TypeOfBusiness("Sale");
     Business business = new Business(200);
-    PublishedAnnouncement p1 = new PublishedAnnouncement(date, typeOfBusiness, property, propertyType, com, business);
-    Message message = new Message(name, phoneNumber, description, date, initialTime, endTime, p1);
+    PublishedAnnouncement p1 = new PublishedAnnouncement(date, typeOfBusiness, property, propertyType, com, business,employee,);
+    Message message = new Message(name, phoneNumber, description, date, initialTime, endTime, p1,MessageState.UNANSWERED,false);
 
 
     @Test
@@ -52,7 +63,7 @@ class SendMessageControllerTest {
         Repositories.getInstance().setMessageRepository(repository);
         controller.messageRepository = repository;
 
-        Optional<Message> result = controller.createNewMessageToAgent(name, description, phoneNumber, date, initialTime, endTime, p1);
+        Optional<Message> result = controller.createNewMessageToAgent(name, description, phoneNumber, date, initialTime, endTime, p1,MessageState.UNANSWERED);
 
         assertTrue(result.isPresent());
         Message message = result.get();
@@ -75,7 +86,7 @@ class SendMessageControllerTest {
         Repositories.getInstance().setMessageRepository(repository);
         controller.messageRepository = repository;
 
-        Optional<Message> result = controller.createNewMessageToAgent(name, description, phoneNumber, date, initialTime, endTime, p1);
+        Optional<Message> result = controller.createNewMessageToAgent(name, description, phoneNumber, date, initialTime, endTime, p1,MessageState.UNANSWERED);
 
         assertFalse(result.isPresent());
     }
