63dfb970e79e3017a0055b724dce4655fc6c7eb7
35193 <1221184@isep.ipp.pt>
2023-06-13 09:37:27 +0100

    PublishedAnnouncementTest ResidenceTest Correction

--- src/test/java/pt/ipp/isep/dei/esoft/project/domain/PublishedAnnouncementTest.java
+++ src/test/java/pt/ipp/isep/dei/esoft/project/domain/PublishedAnnouncementTest.java
@@ -2,23 +2,37 @@ package pt.ipp.isep.dei.esoft.project.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PublishedAnnouncementTest {
 
+    Photos photos = new Photos("urlll");
+    Address address = new Address("123 Main St", 13456, new District("Test District"), new City("Test City"), new State("Test State"));
+    Store store = new Store("Store A", 1, address, 5551234, "storea@example.com",9);
+    Photos photos1 = new Photos("ulll");
+    Address address1 = new Address("12 St", 13426, new District("District"), new City("City"), new State("State"));
+    Photos photos2 = new Photos("ulvrtbll");
+    Address address2 = new Address("12 ihiuech St", 13416, new District("District d"), new City("City fd"), new State("State d"));
+
+    List<Role> roles = new ArrayList<>();
+    Employee employee = new Employee("employee@example.com", 123456789, 987654321, "Name Employee", 5551234, store, roles, new Address("123 Main St", 12345, new District("Test District"), new City("Test City"), new State("Test State")));
+    Client client = new Client("client@this.app",123456789,123456789,"client",address,1234567890L);
+
     private final TypeOfBusiness typeOfBusiness = new TypeOfBusiness("Rent");
     private final PropertyType propertyType = new PropertyType("House");
     private final Comission comission = new Comission(34);
     private final Business business = new Business(100.0);
-    private final Property property = new Property(3455, 56);
+    private final Property property = new Property(3455, 56, photos,address);
     private final int durationOfContract = 6;
     private final Date date = new Date();
 
     @Test
     void testToString() {
-        PublishedAnnouncement announcement = new PublishedAnnouncement(date, typeOfBusiness, property, propertyType, comission, business);
+        PublishedAnnouncement announcement = new PublishedAnnouncement(date, typeOfBusiness, property, propertyType, comission, business,employee, client, 99, AnnouncementState.available,store);
         String expectedString = String.format("Date:%s\n" +
                         "Type of business:%s\n" +
                         "Property Type:%s\n" +
@@ -31,7 +45,7 @@ class PublishedAnnouncementTest {
 
     @Test
     void toStringRent() {
-        PublishedAnnouncement announcement = new PublishedAnnouncement(date, typeOfBusiness, property, propertyType, comission, business, durationOfContract);
+        PublishedAnnouncement announcement = new PublishedAnnouncement(date, typeOfBusiness, property, propertyType, comission, business, durationOfContract,employee, client, 22, AnnouncementState.available,store);
         String expectedString = String.format("Date:%s\n" +
                         "Type of business:%s\n" +
                         "Property Type:%s\n" +
--- src/test/java/pt/ipp/isep/dei/esoft/project/domain/ResidenceTest.java
+++ src/test/java/pt/ipp/isep/dei/esoft/project/domain/ResidenceTest.java
@@ -2,9 +2,24 @@ package pt.ipp.isep.dei.esoft.project.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ResidenceTest {
+    Photos photos = new Photos("urlll");
+    Address address = new Address("123 Main St", 13456, new District("Test District"), new City("Test City"), new State("Test State"));
+    Store store = new Store("Store A", 1, address, 5551234, "storea@example.com",9);
+    Photos photos1 = new Photos("ulll");
+    Address address1 = new Address("12 St", 13426, new District("District"), new City("City"), new State("State"));
+    Photos photos2 = new Photos("ulvrtbll");
+    Address address2 = new Address("12 ihiuech St", 13416, new District("District d"), new City("City fd"), new State("State d"));
+
+    List<Role> roles = new ArrayList<>();
+    Employee employee = new Employee("employee@example.com", 123456789, 987654321, "Name Employee", 5551234, store, roles, new Address("123 Main St", 12345, new District("Test District"), new City("Test City"), new State("Test State")));
+    Client client = new Client("client@this.app",123456789,123456789,"client",address,1234567890L);
+
 
     @Test
     void testToString() {
@@ -15,7 +30,7 @@ class ResidenceTest {
         int parkingSpaces = 1;
         AvailableEquipment availableEquipment = new AvailableEquipment("air conditioning");
 
-        Residence r = new Residence(area, distanceFromCityCenter, numberOfBedrooms, numberOfBathrooms, parkingSpaces, availableEquipment);
+        Residence r = new Residence(area, distanceFromCityCenter, numberOfBedrooms, numberOfBathrooms, parkingSpaces, availableEquipment,photos, address);
 
         String expected = "Area: 100\n" +
                 "DistanceFromCityCenter:10\n" +
@@ -36,8 +51,8 @@ class ResidenceTest {
         int parkingSpaces1 = 1;
         AvailableEquipment availableEquipment1 = new AvailableEquipment("air conditioning");
 
-        Residence r1 = new Residence(area1, distanceFromCityCenter1, numberOfBedrooms1, numberOfBathrooms1, parkingSpaces1, availableEquipment1);
-        Residence r2 = new Residence(area1, distanceFromCityCenter1, numberOfBedrooms1, numberOfBathrooms1, parkingSpaces1, availableEquipment1);
+        Residence r1 = new Residence(area1, distanceFromCityCenter1, numberOfBedrooms1, numberOfBathrooms1, parkingSpaces1, availableEquipment1,photos,address);
+        Residence r2 = new Residence(area1, distanceFromCityCenter1, numberOfBedrooms1, numberOfBathrooms1, parkingSpaces1, availableEquipment1,photos,address);
 
         int area2 = 100;
         int distanceFromCityCenter2 = 10;
@@ -46,7 +61,7 @@ class ResidenceTest {
         int parkingSpaces2 = 1;
         AvailableEquipment availableEquipment2 = new AvailableEquipment("air conditioning");
 
-        Residence r3 = new Residence(area2, distanceFromCityCenter2, numberOfBedrooms2, numberOfBathrooms2, parkingSpaces2, availableEquipment2);
+        Residence r3 = new Residence(area2, distanceFromCityCenter2, numberOfBedrooms2, numberOfBathrooms2, parkingSpaces2, availableEquipment2,photos,address);
 
         //for the same object
         assertEquals(r1, r1);
@@ -65,8 +80,8 @@ class ResidenceTest {
         int parkingSpaces = 1;
         AvailableEquipment availableEquipment = new AvailableEquipment("air conditioning");
 
-        Residence r1 = new Residence(area, distanceFromCityCenter, numberOfBedrooms, numberOfBathrooms, parkingSpaces, availableEquipment);
-        Residence r2 = new Residence(area, distanceFromCityCenter, numberOfBedrooms, numberOfBathrooms, parkingSpaces, availableEquipment);
+        Residence r1 = new Residence(area, distanceFromCityCenter, numberOfBedrooms, numberOfBathrooms, parkingSpaces, availableEquipment,photos,address);
+        Residence r2 = new Residence(area, distanceFromCityCenter, numberOfBedrooms, numberOfBathrooms, parkingSpaces, availableEquipment,photos,address);
 
         assertEquals(r1.hashCode(), r2.hashCode());
     }
