f0884cd25209c5387f41ad9bd8bf597c9cd6ce56
Diogo <1221014@isep.ipp.pt>
2023-06-17 21:18:10 +0100

    US19-adding stores Bootstrap

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/Bootstrap.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/Bootstrap.java
@@ -343,6 +343,7 @@ public class Bootstrap {
 
         storeRepository.add(store1);
 
+
         storeRepository.add(store2);
         storeRepository.add(store3);
         storeRepository.add(store4);
